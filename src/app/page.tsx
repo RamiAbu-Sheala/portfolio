81e13e2db5c87d69644926140f559eaa46b68ff1
Rami Abu-Sheala <rami.abu-sheala@axxes.com>
2024-09-30 17:01:50 +0200

    add scrolllistener

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import {BackgroundLogo} from "@/components/BackgroundLogo";
 import {ToolSet} from "@/pages/toolset/ToolSet";
 import {Experiences} from "@/pages/experiences/Experiences";
 import {Contact} from "@/pages/contact/Contact";
+import {Navbar} from "@/components/Navbar";
+import React from "react";
 
 export default function Home() {
+
     return (
         <div>
+            <Navbar/>
             <WhoAmI/>
             <ToolSet/>
             <Experiences/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import {MdDarkMode} from "react-icons/md";
 import {ScrollLink} from "@/components/ScrollLink";
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 export function Navbar() {
+    const scrollY = useRef<number>(0);
+
     const pages: Map<string, string> = new Map([
         ["#who-am-i", "Who am I?"],
         ["#toolset", "Toolset"],
@@ -23,9 +25,45 @@ export function Navbar() {
         }
     }
 
+    const goToNextSection = () => {
+        const currentSection = document.querySelector(activeSection);
+        if (currentSection) {
+            const nextSection = currentSection.nextElementSibling;
+            if (nextSection) {
+                nextSection.scrollIntoView({behavior: "smooth"});
+                history.pushState(null, "", `#${nextSection.id}`);
+                setActiveSection(`#${nextSection.id}`);
+            }
+        }
+    }
+
+    const handleScroll = (scrollY: number, prevScrollY: number) => {
+        if (scrollY > prevScrollY) {
+            goToNextSection();
+        }
+    }
+//todo: add debounce
+    useEffect(() => {
+        const registerScroll = () => {
+            const currentScrollY = window.scrollY;
+            window.requestAnimationFrame(() => {
+                handleScroll(currentScrollY, scrollY.current);
+                console.log(currentScrollY);
+                console.log(scrollY.current);
+                scrollY.current = currentScrollY;
+            });
+        }
+
+        window.addEventListener('scroll', registerScroll);
+        return () => {
+            window.removeEventListener('scroll', registerScroll);
+        }
+    });
+
     return (
         <div className="fixed w-full z-10 top-0 bg-background-50">
-            <div className="flex justify-between py-4 align-bottom px-8 md:px-24 lg:px-36 xl:px-52 2xl:px-96 3xl:px-128">
+            <div
+                className="flex justify-between py-4 align-bottom px-8 md:px-24 lg:px-36 xl:px-52 2xl:px-96 3xl:px-128">
                 <Image src="logo.svg" alt="logo" width={50} height={50}/>
                 <div className="flex gap-8 align-bottom">
                     {
