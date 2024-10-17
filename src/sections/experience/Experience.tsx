209fea65e54b3f4c5b45cc1475364b2bda7cbc16
Rami Abu-Sheala <rami.abu-sheala@axxes.com>
2024-10-17 16:52:44 +0200

    add 3d cards to projects

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import React from "react";
+
+import Image from "next/image";
 import {Section, SectionProps} from "@/sections";
 import {SectionDescription, SectionHeader, SectionTitle, TitleHighlight} from "@/sections/components";
 import {EVENTS} from "@/sections/experience/Event";
 import {PROJECTS} from "@/sections/experience/Project";
-import {motion} from "framer-motion";
+import {CardBody, CardContainer, CardItem} from "@/components/ui/3d-card";
 
 function Experience({activeSection}: SectionProps) {
     const isActive = activeSection === Section.EXPERIENCE;
@@ -32,17 +35,32 @@ function Experience({activeSection}: SectionProps) {
             </div>
             <div className="grid grid-cols-3 gap-8">
                 {PROJECTS.map((project) => (
-                    <motion.div
-                        key={project.id}
-                        className={"flex flex-col items-center font-NotoSans"}
-                    >
-                        <motion.img
-                            src={project.image}
-                            alt={project.title + " image"}
 
-                        />
-                        <h1>{project.title}</h1>
-                    </motion.div>
+                    <CardContainer className="inter-var" key={project.id}>
+                        <CardBody className="bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border">
+                            <CardItem
+                                translateZ="50"
+                                className="text-xl font-medium font-NotoSans text-neutral-600 dark:text-white"
+                            >
+                                {project.title}
+                            </CardItem>
+                            <CardItem
+                                translateZ="100"
+                                rotateX={20}
+                                rotateY={-4}
+                                className="w-full pt-4"
+                            >
+                                <Image
+                                    src={project.image}
+                                    height="1000"
+                                    width="1000"
+                                    className="object-cover rounded-xl group-hover/card:shadow-xl"
+                                    alt="thumbnail"
+                                />
+                            </CardItem>
+                        </CardBody>
+                    </CardContainer>
+
                 ))}
             </div>
         </div>
